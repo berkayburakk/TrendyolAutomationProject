d859673a60478c44a7563a86c49069801fb4d193
hygiei4 <berkaysan50@hotmail.com>
2021-11-28 20:07:13 +0300

    Third Commit

--- TrendyolAutomationProject/src/test/java/StepDefinitionForAPISearchTest/TestMethodsForSearchAPI.java
+++ TrendyolAutomationProject/src/test/java/StepDefinitionForAPISearchTest/TestMethodsForSearchAPI.java
@@ -1,8 +1,7 @@
-package steps;
+package StepDefinitionForAPISearchTest;
 
 import apiRequests.SearchAPIRequests;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
